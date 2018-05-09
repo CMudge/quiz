2cb54b04c0ac89843215b168212386c96d726b34
CMudge <clairemudge34@gmail.com>
2018-05-09 01:59:21 -0400

    New version with zoom

--- ztnvudg/platforms/browser/www/js/appActivity.js
+++ ztnvudg/platforms/browser/www/js/appActivity.js
@@ -1,12 +1,12 @@
 
 	<!-- the following script will load the map and set the default view and zoom, as well as loading the basemap tiles -->
 
-	// load the map
+	// Load the map - set view and zoom
 	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-	// load the tiles
+	// Load the tiles
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 			maxZoom: 18,
-			//attribution is the copyright statement
+			// Copyright statement
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + 
 				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' + 
 				'Imagery © <a href="http://mapbox.com">Mapbox</a>', 
@@ -14,98 +14,160 @@
 		}).addTo(mymap);
 		
 		
+	
+	
+	
+	
+// function showPosition(position) {
+		// L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
+			// .bindPopup("<b>Position</b><br />Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude).openPopup();
+			// mymap.flyTo([position.coords.latitude, position.coords.longitude], 20);
+// }
 
-	/* create a variable that will hold the XMLHttpRequest() - this must be done outside a function so
-	that all the functions can use the same variable */
-	var client
-	/* and a variable that will hold the layer itself – we need to do this outside the function so that we
-	can use it to remove the layer later on */
-	var earthquakelayer;
-	
-	// create the code to get the Earthquakes data using an XMLHttpRequest
-	function getEarthquakes() {
-		client = new XMLHttpRequest();
 
-	client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
-		client.onreadystatechange = earthquakeResponse;
-		// note don't use earthquakeResponse() with brackets as that doesn't work
-		client.send();
-	}
-	
-	/* create the code to wait for the response from the data server, and process the response once 
-	it is received */
-	function earthquakeResponse() {
-		// this function listens out for the server to say that the data is ready - i.e. has state 4
-		if (client.readyState == 4) {
-			// once the data is ready, process the data
-			var earthquakedata = client.responseText;
-			loadEarthquakelayer(earthquakedata);
-			}
-	}
-	// convert the received data - which is text - to JSON format and add it to the map
-	function loadEarthquakelayer(earthquakedata) {
-		// convert the text to JSON
-		var earthquakejson = JSON.parse(earthquakedata);
-		// add the JSON layer onto the map - it will appear using the default icons
-		var earthquakelayer = L.geoJson(earthquakejson).addTo(mymap);
-		// change the map zoom so that all the data is shown
-		mymap.fitBounds(earthquakelayer.getBounds());
-	}
 
-	
-	
-	
-	
-	
-		/* create a variable that will hold the XMLHttpRequest() - this must be done outside a function so
-	that all the functions can use the same variable */
-	var client
-	/* and a variable that will hold the layer itself – we need to do this outside the function so that we
-	can use it to remove the layer later on */
-	var poilayer;
-	
-	// create the code to get the Earthquakes data using an XMLHttpRequest
-	function getPoi() {
-		client = new XMLHttpRequest();
+var question;
 
-	client.open('GET','http://developer.cege.ucl.ac.uk:30266/getGeoJSON/united_kingdom_highway/geom');
-		client.onreadystatechange = poiResponse;
-		// note don't use earthquakeResponse() with brackets as that doesn't work
-		client.send();
-	}
-	
-	/* create the code to wait for the response from the data server, and process the response once 
-	it is received */
-	function poiResponse() {
-		// this function listens out for the server to say that the data is ready - i.e. has state 4
-		if (client.readyState == 4) {
-			// once the data is ready, process the data
-			var poidata = client.responseText;
-			loadPoilayer(poidata);
-			}
-	}
-	// convert the received data - which is text - to JSON format and add it to the map
-	function loadPoilayer(poidata) {
-		// convert the text to JSON
-		var poijson = JSON.parse(poidata);
-		// add the JSON layer onto the map - it will appear using the default icons
-		var poilayer = L.geoJson(poijson).addTo(mymap);
-		// change the map zoom so that all the data is shown
-		mymap.fitBounds(poilayer.getBounds());
-	}
+// Function to retrieve and show the question text and display the location of the question
+function questionShow() {
+  questions = JSON.parse(client.responseText);
+  question = questions[0]['features'][0]
+  navigator.geolocation.clearWatch(watcherId);
+  if (client.readyState == 4) {
+    // change the DIV to show the question
+    document.getElementById("question").innerHTML = question["properties"]["question"]+
+		'	<br />'+
+		'	<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="correctAnswer1">'+
+		'    <input type="radio" id="correctAnswer1" class="mdl-radio__button" name="options" value="question["properties"]["answer1"]">'+
+		'		<span class="mdl-radio__label">'+question["properties"]["answer1"]+'</span>'+
+		'	</label>'+
+		'	<br />'+
+		'	<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="correctAnswer2">'+
+		'	  <input type="radio" id="correctAnswer2" class="mdl-radio__button" name="options" value="question["properties"]["answer2"]">'+
+		'	  <span class="mdl-radio__label">'+question["properties"]["answer2"]+'</span>'+
+		'	</label>'+
+		'	<br />'+
+		'	<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="correctAnswer3">'+
+		'	  <input type="radio" id="correctAnswer3" class="mdl-radio__button" name="options" value="question["properties"]["answer3"]">'+
+		'	  <span class="mdl-radio__label">'+question["properties"]["answer3"]+'</span>'+
+		'	</label>'+
+		'	<br />'+
+		'	<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="correctAnswer4">'+
+		'	  <input type="radio" id="correctAnswer4" class="mdl-radio__button" name="options" value="question["properties"]["answer4"]">'+
+		'	  <span class="mdl-radio__label">'+question["properties"]["answer4"]+'</span>'+
+		'	</label>'+
+		'	<br />'+
+		'		<div class="mdl-card__actions mdl-card--border">'+
+		'		<a onclick="startDataUpload()" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--blue-grey-500">Submit Answer</a>'+
+        '</div>';
+		
+	// Show the question location as a marker on the map 
+	var marker = L.marker();
+	// Set the latitude and longitude coordinates to the question latitude and longitude
+	var latlng = [question['geometry']['coordinates'][1], question['geometry']['coordinates'][0]]; 
+	marker
+		.setLatLng(latlng)
+		.addTo(mymap);
+	mymap.setView(latlng, 18);
+	<!-- client.send(); -->
+    }
+}
 
-	
-	
-/*function trackLocation() {
+var watcherId;
+
+// Function to track the user's location
+function trackLocation() {
  if (navigator.geolocation) {
-     navigator.geolocation.watchPosition(showPosition);
+     watcherId = navigator.geolocation.watchPosition(requestNearestQuestion);
  } else {
-     alert('Geolocation is not supported by this browser.');
+     document.getElementById('question').innerHTML = "Geolocation is not supported by this browser.";
  }
 }
-*/
-function showPosition(position) {
-		L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
-			.bindPopup("<b>Position</b><br />Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude).openPopup();
-			mymap.flyTo([position.coords.latitude, position.coords.longitude], 20);
+
+// Function to request the nearest question based on the user's location
+function requestNearestQuestion(position) {
+  var requestString = "lat="+ position.coords.latitude +"&lng="+position.coords.longitude;
+  client = new XMLHttpRequest();
+  var url = "http://developer.cege.ucl.ac.uk:30266/closestQuestion?" + requestString;
+  client.open('GET', url, true);
+  //client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  client.onreadystatechange = questionShow;
+  client.send();
+}
+
+// Function to send the user's answer to the server
+function processData(postString) {
+	client = new XMLHttpRequest();
+	client.open('POST','http://developer.cege.ucl.ac.uk:30266/submitAnswer',true);
+ 
+	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	// Code adapted from https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/onreadystatechange
+	client.onreadystatechange = function () {
+		if(client.readyState === 4 && client.status === 200) {
+			console.log("Answer submitted successfully");
+		}
+	};
+   client.send(postString);
+}
+
+function onDeviceReady() {
+    console.log(device.cordova);
+}
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Function to extract user's answer
+function startDataUpload() {
+	questionText = question['properties']['question'];
+	submittedAnswer = 0;
+	mobileUUID = 0;
+
+	// Determine which is the answer chosen by the user
+	if (document.getElementById("correctAnswer1").checked === true) {
+		submittedAnswer = 1;
+	}
+	else if (document.getElementById("correctAnswer2").checked === true) {
+		submittedAnswer = 2;
+	}
+	else if (document.getElementById("correctAnswer3").checked === true) {
+		submittedAnswer = 3;
+	}
+	else if (document.getElementById("correctAnswer4").checked === true) {
+		submittedAnswer = 4;
+	}
+	// Notify the user of the correct answer
+	if (submittedAnswer == question['properties']['correctanswer']) {
+		document.getElementById("answerOutcome").innerHTML = "That's correct!";
+	}
+	else {
+		document.getElementById("answerOutcome").innerHTML = "That's incorrect. The correct answer is "+question['properties']['correctanswer'];
+	}
+	//Set up variables from parameters
+	var postString = "question="+question+"&submittedAnswer="+submittedAnswer+"&mobileUUID="+mobileUUID;
+	processData(postString);
+}
+
+
+// Function to dynamically change the main div to the User Guide
+var xhr; // define the global variable to process the AJAX request
+function callDivChange() {
+	xhr = new XMLHttpRequest();
+	xhr.open("GET", "quizUserGuide.html", true);
+	xhr.onreadystatechange = processDivChange;
+	try {
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	}
+	catch (e) {
+		// this only works in internet explorer
+	}
+	xhr.send();
+}
+
+function processDivChange() {
+	if (xhr.readyState < 4) // while waiting response from server
+		document.getElementById('question').innerHTML = "Loading...";
+	else if (xhr.readyState === 4) { // 4 = Response from server has been completely loaded.
+		if (xhr.status == 200 && xhr.status < 300)
+			// http status between 200 to 299 are all successful
+			document.getElementById('main').innerHTML = xhr.responseText;
+	}
 }
